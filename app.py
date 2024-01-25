869c57aa55e630fb050dd2ef1a02f5650e38d950
HarryW217 <harrywalker217@gmail.com>
2024-01-25 16:19:28 +0000

    Created a dynamic route that returns Hello followed by the name in the URL, and implemented debugger that provides error messages when running the app programatically

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi, {}'.format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
