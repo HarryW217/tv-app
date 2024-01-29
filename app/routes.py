8a153a6df86a874f7677e94cfb1a55d4357b0b59
HarryW217 <harrywalker217@gmail.com>
2024-01-29 16:07:18 +0000

    Imported some modules in our routes.py file to aid with loggin in and logging out. We have started by coding the logout route in this file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
-from flask import render_template
-from app import app
+from flask import render_template, url_for, redirect
+from app import app, db
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
+from app.models import *
+
+#### LOGIN ####
+login_manager = LoginManager()
+login_manager.init.app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#### LOGIN / LOGOUT ####
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
