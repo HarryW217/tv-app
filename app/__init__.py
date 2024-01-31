edfb1786adade99b8b5126eeb891a8581d491d00
HarryW217 <harrywalker217@gmail.com>
2024-01-31 15:59:31 +0000

    Added Secret Key to use Cross-Site Request Forgery

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
