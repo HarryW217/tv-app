41294579e1de83c5a64af30f8b922442684f60fd
HarryW217 <harrywalker217@gmail.com>
2024-01-31 15:25:23 +0000

    Created login route in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, flash
 from app import app, db
 from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from app.models import *
+from app.forms import LoginForm
 
 #### LOGIN ####
 login_manager = LoginManager()
@@ -16,6 +17,23 @@ def index():
     return render_template('index.html')
 
 #### LOGIN / LOGOUT ####
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    
+    # get form data
+    form_username = form.username.data
+    form_password = form.password.data
+    
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_name=form_username).first()
+        password = user.password
+        if user is None or password != form_password:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
 
 @app.route('/logout')
 @login_required
